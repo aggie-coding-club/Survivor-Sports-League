c2416d91147e5b7fe516f716a0c595572d2a5842
eoe789 <airplane789@tamu.edu>
2024-10-25 18:03:44 -0500

    api printing in terminal

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/', methods=['GET'])
+def index():
+    headers = {"accept": "application/json"}    
+    url = "https://api.sportradar.com/nfl/official/trial/v7/en/games/2024/REG/05/schedule.json?api_key=M31MHThjj9azPcbv3OTqSs3mSTWTKSz8VMthJGrZ"
+
+    req = requests.get(url, headers)
+    print(req.content)
+    data=json.loads(req.content)
+    return render_template('index.html', data=data)
